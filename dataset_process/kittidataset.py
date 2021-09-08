d7cb0c5768f9f3754a032d0f78e60fba08e2b879
jingyuSong <68323609+sjg918@users.noreply.github.com>
2021-09-09 03:03:05 +0900

    Update kittidataset.py

--- dataset_process/kittidataset.py
+++ dataset_process/kittidataset.py
@@ -5,39 +5,17 @@ from .utils import *
 import os
 import numpy as np
 
-_BASE_DIR = 'C:\Datasets/sample/'
+_BASE_DIR = './samples/'
 
 class KittiDataset(data.Dataset):
     def __init__(self, cfg, mode='train'):
         self.mode = mode
         with open(_BASE_DIR + 'train.txt') as f:
             self.lists = f.readlines()
-        self.pointroi = BoundaryCheck(cfg)
         self.point2voxel = Point2Voxel(cfg)
         self.randommosaic = RandomMosaic(cfg)
     def __len__(self):
         return len(self.lists)
-
-    def read_velo(self, filename):
-        points = np.fromfile(filename, dtype=np.float32).reshape(-1, 4)
-        return points
-
-    def read_label(self, filename):
-        lines = [line.rstrip() for line in open(filename)]
-        objects = [Object2Cam(line) for line in lines]
-        olist = []
-        for o in objects:
-            if o.cls_id == -1:
-                continue
-            else:
-                olist.append(o)
-        if len(olist) > 0:
-            return olist
-        else:
-            return None
-
-    def read_calib(self, filename):
-        return Calibration(filename)
 
     def __getitem__(self, idx):
 
@@ -47,9 +25,9 @@ class KittiDataset(data.Dataset):
         Plist, Llist, Clist = [], [], []
         for i in range(4):
             id = random.randint(0, self.__len__() - 1)
-            points = self.read_velo(_BASE_DIR + 'velodyne(lidar16ch)/' + self.lists[id][:6] + '.bin')
-            labels = self.read_label(_BASE_DIR + 'label/' + self.lists[id][:6] + '.txt')
+            points = self.read_velo(_BASE_DIR + 'velodyne(lidar)/' + self.lists[id][:6] + '.bin')
             calib  = self.read_calib(_BASE_DIR + 'calib/' + self.lists[id][:6] + '.txt')
+            labels = self.read_label(_BASE_DIR + 'label/' + self.lists[id][:6] + '.txt', points, calib)
             Plist.append(points)
             Llist.append(labels)
             Clist.append(calib)
@@ -60,3 +38,26 @@ class KittiDataset(data.Dataset):
         
         kew = self.randommosaic(Plist, Llist, Clist)
         return 0
+
+    def read_velo(self, filepath):
+        points = KittiScene(filepath)
+        return points
+
+    def read_calib(self, filename):
+        return Calibration(filename)
+
+    def read_label(self, filename, points, calib):
+        lines = [line.rstrip() for line in open(filename)]
+        objects = [KittiObject(line, points.points, calib) for line in lines]
+        olist = []
+        for o in objects:
+            if o.cls_id == -1:
+                continue
+            elif o.level == 4:
+                continue
+            else:
+                olist.append(o)
+        if len(olist) > 0:
+            return olist
+        else:
+            return None
