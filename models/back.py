a22343cf1f5e564c515b2b8947853b3d0ecc2d84
jingyuSong <68323609+sjg918@users.noreply.github.com>
2021-09-16 21:27:51 +0900

    Update back.py

--- models/back.py
+++ models/back.py
@@ -39,10 +39,12 @@ class VFELayer(nn.Module):
 class VoxelFeatureExtractor(nn.Module):
     def __init__(self, cfg):
         super(VoxelFeatureExtractor, self).__init__()
+        self.back_bev_init_shape = cfg.back_bev_init_shape
         self.in_channels = cfg.back_in_channels
         self.voxelwise_channels = cfg.back_voxelwise_channels
-        self.bevin_channels = cfg.v2b_maxvoxels
+        self.bevin_channels = cfg.back_bev_in_channels
         self.bev_channels = cfg.back_bev_channels
+        self.bevshape = cfg.bevshape
 
         self.vfe1 = VFELayer(self.in_channels, self.voxelwise_channels//4)
         self.vfe2 = VFELayer(self.voxelwise_channels//4, self.voxelwise_channels)
@@ -91,57 +93,52 @@ class VoxelFeatureExtractor(nn.Module):
             nn.BatchNorm1d(self.bev_channels),
             nn.Mish())
 
-    def forward(self, features, coors, num_voxels):
+    def forward(self, voxels, voxelmask, bevsidx, bevcoors, bevmask):
         # step1.
-        voxel_per_points = features.shape[1]
-        mask = self.get_paddings_indicator(num_voxels, voxel_per_points, axis=0)
-        mask = mask.unsqueeze(-1).to(torch.float32).to(features.device)
-        x1 = self.vfe1(features)
-        x2 = self.vfe2(x1) * mask
+        x1 = self.vfe1(voxels)
+        x2 = self.vfe2(x1) * voxelmask
         x3 = self.vfe3(x2)
-        x4 = self.vfe4(x3) * mask + x2
+        x4 = self.vfe4(x3) * voxelmask + x2
         voxelwise = torch.max(x4, dim=1)[0]
         
         # step2.
-        bevs, bevcoors, num_voxels_ = self.voxel2bev(voxelwise, coors, self.bevin_channels, 64)
-        x0 = bevs.to(features.device).permute(0, 2, 1).contiguous()
+        bevs = torch.zeros((bevsidx.shape[0], self.bevin_channels, self.voxelwise_channels),\
+           dtype=torch.float32, device=voxels.device)
+        bevs[:, :] = voxelwise[bevsidx[:, :]]
         
         # step3.
-        bevs_per_voxels = bevs.shape[1]
-        mask = self.get_paddings_indicator(num_voxels_, bevs_per_voxels, axis=0)
-        mask = mask.unsqueeze(dim=1).to(torch.float32).to(features.device)
-        
-        x1 = self.bfe1_01(x0[:, 0:4, :]) * mask
+        x0 = bevs.permute(0, 2, 1).contiguous()
+        x1 = self.bfe1_01(x0[:, 0:4, :]) * bevmask
         x1 = self.bfe2_01(x1)
-        x2 = self.bfe1_02(x0[:, 4:8, :]) * mask
+        x2 = self.bfe1_02(x0[:, 4:8, :]) * bevmask
         x2 = self.bfe2_02(x2)
-        x3 = self.bfe1_03(x0[:, 8:12, :]) * mask
+        x3 = self.bfe1_03(x0[:, 8:12, :]) * bevmask
         x3 = self.bfe2_03(x3)
-        x4 = self.bfe1_04(x0[:, 12:16, :]) * mask
+        x4 = self.bfe1_04(x0[:, 12:16, :]) * bevmask
         x4 = self.bfe2_04(x4)
-        x5 = self.bfe1_05(x0[:, 16:20, :]) * mask
+        x5 = self.bfe1_05(x0[:, 16:20, :]) * bevmask
         x5 = self.bfe2_05(x5)
-        x6 = self.bfe1_06(x0[:, 20:24, :]) * mask
+        x6 = self.bfe1_06(x0[:, 20:24, :]) * bevmask
         x6 = self.bfe2_06(x6)
-        x7 = self.bfe1_07(x0[:, 24:28, :]) * mask
+        x7 = self.bfe1_07(x0[:, 24:28, :]) * bevmask
         x7 = self.bfe2_07(x7)
-        x8 = self.bfe1_08(x0[:, 28:32, :]) * mask
+        x8 = self.bfe1_08(x0[:, 28:32, :]) * bevmask
         x8 = self.bfe2_08(x8)
-        x9 = self.bfe1_09(x0[:, 32:36, :]) * mask
+        x9 = self.bfe1_09(x0[:, 32:36, :]) * bevmask
         x9 = self.bfe2_09(x9)
-        x10 = self.bfe1_10(x0[:, 36:40, :]) * mask
+        x10 = self.bfe1_10(x0[:, 36:40, :]) * bevmask
         x10 = self.bfe2_10(x10)
-        x11 = self.bfe1_11(x0[:, 40:44, :]) * mask
+        x11 = self.bfe1_11(x0[:, 40:44, :]) * bevmask
         x11 = self.bfe2_11(x11)
-        x12 = self.bfe1_12(x0[:, 44:48, :]) * mask
+        x12 = self.bfe1_12(x0[:, 44:48, :]) * bevmask
         x12 = self.bfe2_12(x12)
-        x13 = self.bfe1_13(x0[:, 48:52, :]) * mask
+        x13 = self.bfe1_13(x0[:, 48:52, :]) * bevmask
         x13 = self.bfe2_13(x13)
-        x14 = self.bfe1_14(x0[:, 52:56, :]) * mask
+        x14 = self.bfe1_14(x0[:, 52:56, :]) * bevmask
         x14 = self.bfe2_14(x14)
-        x15 = self.bfe1_15(x0[:, 56:60, :]) * mask
+        x15 = self.bfe1_15(x0[:, 56:60, :]) * bevmask
         x15 = self.bfe2_15(x15)
-        x16 = self.bfe1_16(x0[:, 60:64, :]) * mask
+        x16 = self.bfe1_16(x0[:, 60:64, :]) * bevmask
         x16 = self.bfe2_16(x16)
 
         x17 = torch.cat([x1,x2,x3,x4,x5,x6,x7,x8,
@@ -150,8 +147,9 @@ class VoxelFeatureExtractor(nn.Module):
         
         # step4.
         x18 = self.bfe3(x17)
-        x19 = self.bfe3(x18)
-        bevmap = torch.zeros((self.bevshape[0], self.bevshape[1], 128), dtype=torch.float32, device=x17.device)
+        x19 = self.bfe3(x18) + x17
+        bevmap = torch.zeros((self.bevshape[0], self.bevshape[1], 128),
+                             dtype=torch.float32, device=voxels.device)
         bevmap[bevcoors[:, 0], bevcoors[:, 1]] = x19
         
         return bevmap.permute(2, 1, 0).contiguous()
