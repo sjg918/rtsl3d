46b22d7449df5c23f9e7a39d2faf3dc1730e9acb
jingyuSong <68323609+sjg918@users.noreply.github.com>
2021-09-16 19:59:40 +0900

    Update CFG.py

--- CFG.py
+++ CFG.py
@@ -15,5 +15,18 @@ cfg.maxZ = 1
 cfg.voxelsize = (0.1, 0.1, 0.1)
 cfg.voxelrange = (0, -30.0, -3.0, 72.0, 30.0, 1.0)
 cfg.voxelshape = (40, 600, 720)
-cfg.maxpoints = 16
-cfg.maxvoxels = 20000
+cfg.p2v_maxpoints = 16
+cfg.p2v_maxvoxels = 100000
+
+# setting for voxel2bev
+cfg.bevsize = (0.5, 0.5)
+cfg.bevmulti = (int(cfg.bevsize[0] / cfg.voxelsize[1]), int(cfg.bevsize[1] / cfg.voxelsize[2]))
+cfg.bevshape = (cfg.voxelshape[1]//cfg.bevmulti[0], cfg.voxelshape[2]//cfg.bevmulti[1])
+cfg.v2b_maxvoxels = cfg.voxelshape[0] * cfg.bevmulti[0] * cfg.bevmulti[1]
+
+# setting for back_network
+cfg.back_in_channels = 7
+cfg.back_voxelwise_channels = 64
+cfg.back_bev_in_channels = cfg.v2b_maxvoxels
+cfg.back_bev_init_shape = ()
+cfg.back_bev_channels = 128
